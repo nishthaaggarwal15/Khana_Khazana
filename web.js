e698e7f556b83804612d9ee36c296ef6553e47a6
nishthaaggarwal15 <nishthaaggarwal15@gmail.com>
2024-04-28 15:48:48 +0530

    Updated

---
+++ web.js
@@ -0,0 +1,96 @@
+var count = 0;
+var amount1 =0;
+var items="";
+function addtocart1() {
+
+ count = count + 1;
+ amount1= amount1 + 1200;
+ items= items + "Pizza"
+ document.getElementById("cart").innerHTML = " items."+items;
+
+
+   var cart = document.getElementById("cart")
+   cart.innerText  = count
+
+   var amount = document.getElementById("amt")
+   amount.innerText = amount1
+   
+}
+
+
+
+var count = 0;
+var amount1 =0;
+
+function addtocart2() {
+
+ count = count + 1;
+ amount1= amount1 + 500;
+
+
+   var cart = document.getElementById("cart")
+   cart.innerText  = count
+
+   var amount = document.getElementById("amt")
+   amount.innerText = amount1
+}
+
+
+var count = 0;
+var amount1 =0;
+
+function addtocart3() {
+
+ count = count + 1;
+ amount1= amount1 + 1000;
+
+
+   var cart = document.getElementById("cart")
+   cart.innerText  = count
+
+   var amount = document.getElementById("amt")
+   amount.innerText = amount1
+}
+
+var count = 0;
+var amount1 =0;
+
+function addtocart4() {
+
+ count = count + 1;
+ amount1= amount1 + 800;
+
+
+   var cart = document.getElementById("cart")
+   cart.innerText  = count
+
+   var amount = document.getElementById("amt")
+   amount.innerText = amount1
+}
+
+var count = 0;
+var amount1 =0;
+
+function addtocart5() {
+
+ count = count + 1;
+ amount1= amount1 + 1500;
+
+
+   var cart = document.getElementById("cart")
+   cart.innerText  = count
+
+   var amount = document.getElementById("amt")
+   amount.innerText = amount1
+}
+
+
+
+
+
+
+
+
+
+
+
